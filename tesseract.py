b2f49567b17db03ac5747ed8575089809e1d5b35
Ankit404butfound <ankitrajmahapatra@gmail.com>
2023-07-06 13:20:29 +0530

    Add ui

--- tesseract.py
+++ tesseract.py
@@ -1,11 +1,12 @@
-import cv2
 from PIL import Image
-import pytesseract
 from pdf2image import convert_from_path
-import numpy as np
+from tkinter.filedialog import askopenfilename, askdirectory
+from tkinter import messagebox
 
-import cv2
 import numpy as np
+import cv2
+import tkinter
+# import pytesseract
 import uuid
 
 # Adding custom options
@@ -50,9 +51,10 @@ def clean_image(image):
 
 
 
-def pdf_to_text(pdf_path, language):
+def pdf_to_text(pdf_path, output_path, language):
     # Convert PDF to images
-    images = convert_from_path(pdf_path)
+    print(pdf_path, output_path, language)
+    images = convert_from_path(pdf_path, output_path)
 
     # Process each image
     for i, image in enumerate(images):
@@ -71,7 +73,7 @@ def pdf_to_text(pdf_path, language):
                 f.write(f'--- Page {i+1} ---')
                 f.write(text)
 
-def convert(path):
+def convert(path, output_path):
     img=Image.open(path)
     img=np.array(img)
     #print(img)
@@ -82,27 +84,67 @@ def convert(path):
     with open(output_path, 'w',encoding='UTF-8') as f:
         f.write(text)
 
-# Path to your pdf
-file_path = input('Enter the path to the PDF file: ')
-# Path to your output text file
-output_path = input('Enter the path to save the output text file: ')
-print('\nSelect the language:')
-print('For Telugu Type: tel')
-print('For English Type: eng')
-print('For Oriya Type: ori\n')
 
-language = input('Enter the Input language of the pdf\n')
+def main(file_path, output_path, language):
+    output_path = output_path + "/" + file_path.split("/")[-1].split(".")[0] + ".txt"
+    print(file_path, output_path, language)
+    try:
+        with open(file_path, 'rb') as f:
+            pdf_to_text(file_path, output_path, language)
+            pass
+    except FileNotFoundError:
+        messagebox.showerror("Error", f"File not found: {file_path}")
+    except PermissionError:
+        messagebox.showerror("Error", "Permission denied")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {e}")
 
-#convert(file_path,language)
+# File selection button
+def file_selection(selection):
+    if selection == "input":
+        file_path = askopenfilename(filetypes=[("PDF Files", "*.pdf")])
+        input_file_selection_button.config(text=file_path)
+    elif selection == "output":
+        output_path = askdirectory()
+        output_file_selection_button.config(text=output_path)
 
-try:
-    with open(file_path, 'rb') as f:
-        pdf_to_text(file_path,language)
-        pass
-except FileNotFoundError:
-    print(f"File not found: {file_path}")
-except PermissionError:
-    print(f"Permission denied: {file_path}")
-except Exception as e:
-    print(f"An error occurred: {e}")
+# Tkinter app to accept file_path, output_path and langugae dropdown
+
+root = tkinter.Tk()
+root.title("PDF to Text")
+# root.geometry("500x300")
+
+# Create a Tkinter variable
+tkvar = tkinter.StringVar(root)
+
+# Dictionary with options
+choices = { 'tel','eng','ori'}
+tkvar.set('tel') # set the default option
+
+popupMenu = tkinter.OptionMenu(root, tkvar, *choices)
+tkinter.Label(root, text="Choose a language").grid(row = 0, column = 0)
+popupMenu.grid(row = 0, column =1)
+
+intpu_file_path_label = tkinter.Label(root, text="Select Input file: ")
+intpu_file_path_label.grid(row=1, column=0)
+
+input_file_selection_button = tkinter.Button(root, text="--Select--", command=lambda : file_selection("input"))
+input_file_selection_button.grid(row=1, column=1)
+
+output_file_path_label = tkinter.Label(root, text="Select Output Folder: ")
+output_file_path_label.grid(row=2, column=0)
+
+output_file_selection_button = tkinter.Button(root, text="--Select--", command=lambda : file_selection("output"))
+output_file_selection_button.grid(row=2, column=1)
+
+submit_button = tkinter.Button(root, text="Submit", command=lambda : main(input_file_selection_button.cget("text"), output_file_selection_button.cget("text"), tkvar.get()))
+submit_button.grid(row=3, column=1)
+
+root.mainloop()
+
+
+
+
+
+
 
